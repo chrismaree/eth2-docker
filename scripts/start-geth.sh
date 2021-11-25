5f60bb5a35bb1789896f585a2e3c35cd92cada95
Christopher Maree <christopher.maree@gmail.com>
2021-11-25 14:11:31 +0200

    updated config
    Signed-off-by: Christopher Maree <christopher.maree@gmail.com>

--- scripts/start-geth.sh
+++ scripts/start-geth.sh
@@ -15,8 +15,8 @@ fi
 
 if [ "$START_GETH" != "" ]; then
 	if [ "$NETWORK" != "$DEFAULT_NETWORK" ]; then
-		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable --nousb $METRICS_PARAMS
+		exec geth --goerli --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable $METRICS_PARAMS
 	else
-		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable --nousb $METRICS_PARAMS snapshot prune-state
+		exec geth --http --http.addr "0.0.0.0" --http.vhosts=* --http.api "eth,net" --ipcdisable $METRICS_PARAMS
 	fi
 fi
